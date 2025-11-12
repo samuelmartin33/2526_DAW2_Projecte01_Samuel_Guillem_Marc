d4be1bf8c20b73a8965ec1e2f0f4f75562137148
samuelmartin <samuelmartin991@gmail.com>
2025-11-12 17:48:32 +0100

    favicon en todas las paginas

--- PHP/PUBLIC/SALAS/comedor1.php
+++ PHP/PUBLIC/SALAS/comedor1.php
@@ -68,6 +68,7 @@ try {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="icon" type="image/png" href="../../../img/icono.png">
 
     <link rel="stylesheet" href="../../../css/panel_principal.css">
     <link rel="stylesheet" href="../../../css/salas_general.css">
--- PHP/PUBLIC/index.php
+++ PHP/PUBLIC/index.php
@@ -101,6 +101,8 @@ if ($hora >= 6 && $hora < 12) {
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../../css/panel_principal.css">
+    <link rel="icon" type="image/png" href="../../img/icono.png">
+
 </head>
 <body>
     
