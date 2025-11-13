a1216b0e097e6214f6e68af2c20fa80ef7cfa0ac
samuelmartin <samuelmartin991@gmail.com>
2025-11-13 17:04:20 +0100

    sweetalert de ocupar mesa

--- PHP/PROCEDIMIENTOS/liberar_mesa.php
+++ PHP/PROCEDIMIENTOS/liberar_mesa.php
@@ -111,6 +111,8 @@ try {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <script src="../PUBLIC/JS/salas.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <link rel="stylesheet" href="../../css/panel_principal.css">
     <link rel="stylesheet" href="../../css/salas_general.css">
